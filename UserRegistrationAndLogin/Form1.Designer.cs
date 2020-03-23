c519ecde5f8e7b42cc9fcb0144de4070fd97f93f
TakiTazwar <takitazwarparthib@gmail.com>
2020-03-23 08:54:55 +0600

    Update Change

--- UserRegistrationAndLogin/Form1.Designer.cs
+++ UserRegistrationAndLogin/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.txtEmailAddress = new System.Windows.Forms.TextBox();
             this.btnSave = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonshow = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -108,11 +109,21 @@
             this.dataGridView1.Size = new System.Drawing.Size(338, 150);
             this.dataGridView1.TabIndex = 7;
             // 
+            // buttonshow
+            // 
+            this.buttonshow.Location = new System.Drawing.Point(302, 349);
+            this.buttonshow.Name = "buttonshow";
+            this.buttonshow.Size = new System.Drawing.Size(75, 23);
+            this.buttonshow.TabIndex = 8;
+            this.buttonshow.Text = "Show Button";
+            this.buttonshow.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonshow);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnSave);
             this.Controls.Add(this.label3);
@@ -140,6 +151,7 @@
         private System.Windows.Forms.TextBox txtEmailAddress;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonshow;
     }
 }
 
