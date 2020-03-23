ff2ee0e704fbc05c6e4670cf0f6c4b69aba2e0c0
TakiTazwar <takitazwarparthib@gmail.com>
2020-03-23 09:08:43 +0600

    Check box added
    ABC

--- UserRegistrationAndLogin/Form1.Designer.cs
+++ UserRegistrationAndLogin/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.btnSave = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.buttonshow = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -118,11 +119,22 @@
             this.buttonshow.Text = "Show Button";
             this.buttonshow.UseVisualStyleBackColor = true;
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(111, 191);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.buttonshow);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.btnSave);
@@ -152,6 +164,7 @@
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button buttonshow;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
